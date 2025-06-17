c6bacbd729a98341be041cf1ae1d5d987908deff
Alaamuhammeddd <alaamuhammeddd17@gmail.com>
2025-06-17 20:05:34 +0300

    header&footer

---
+++ src/Shared/Footer.vue
@@ -0,0 +1,151 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="footer">
+    <div class="main-footer">
+      <div class="left-footer">
+        <div class="first-col">
+          <h5>Navigate</h5>
+          <ul class="footer_links">
+            <li>About us</li>
+            <li>Blog</li>
+            <li>Contact Us</li>
+            <li>Site Map</li>
+          </ul>
+        </div>
+        <div class="second-col">
+          <h5>Collection</h5>
+          <ul class="footer_links">
+            <li>New Arrivals</li>
+            <li>Features</li>
+            <li>Catalog</li>
+            <li>Brands</li>
+          </ul>
+        </div>
+        <div class="third-col">
+          <h5>Categories</h5>
+          <ul class="footer_links">
+            <li>Tables</li>
+            <li>Lamps</li>
+            <li>Chairs</li>
+            <li>Sofas</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="fourth-col">
+        <p class="address">399 Crowfield Rd, Phoenix, Arizona 85012</p>
+        <a href="blank">asff@fddfd.com</a>
+        <p>+201060618854</p>
+      </div>
+    </div>
+    <div class="socials-footer">
+      <p>Copyright &copy; All Rights Reserved</p>
+      <div class="socialMedia-icons">
+        <p>Get Socials</p>
+        <button class="socialMedia-icons__social-btn">
+          <i class="fa fa-twitter-square"></i>
+        </button>
+        <button class="socialMedia-icons__social-btn">
+          <i class="fa fa-instagram"></i>
+        </button>
+        <button class="socialMedia-icons__social-btn">
+          <i class="fa fa-pinterest"></i>
+        </button>
+        <button class="socialMedia-icons__social-btn">
+          <i class="fa fa-facebook"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@media (max-width: 1023px) {
+  .footer {
+    display: flex;
+    flex-direction: column;
+    background-color: bisque;
+    font-size: 21px;
+  }
+  .left-footer {
+    display: flex;
+    gap: 40px;
+    justify-content: space-evenly;
+    text-align: center;
+    font-size: 20px;
+  }
+  .footer_links {
+    list-style-type: none;
+    column-gap: 10px;
+    align-items: center;
+    text-align: center;
+  }
+  .footer_links > li {
+    padding-block: 10px;
+  }
+
+  .socialMedia-icons__social-btn {
+    border: 0;
+    background: none;
+    font-size: 28px;
+    &:hover {
+      cursor: pointer;
+      color: grey;
+    }
+  }
+  .socials-footer {
+    display: flex;
+    justify-content: space-between;
+    font-size: 21px;
+  }
+}
+@media (min-width: 1024px) {
+  .left-footer {
+    display: flex;
+    gap: 40px;
+    justify-content: space-evenly;
+    text-align: center;
+    font-size: 20px;
+  }
+  .socialMedia-icons__social-btn {
+    border: 0;
+    background: none;
+    font-size: 28px;
+    &:hover {
+      cursor: pointer;
+      color: grey;
+    }
+  }
+  .socials-footer {
+    display: flex;
+    justify-content: space-between;
+    font-size: 21px;
+  }
+  .socialMedia-icons {
+    display: flex;
+  }
+  .main-footer {
+    background-color: bisque;
+    display: flex;
+    justify-content: space-around;
+    font-size: 21px;
+  }
+  .footer_links {
+    list-style-type: none;
+    column-gap: 10px;
+    align-items: center;
+    text-align: center;
+  }
+  .footer_links > li {
+    padding-block: 10px;
+  }
+}
+</style>
---
+++ src/Shared/Header.vue
@@ -0,0 +1,139 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
+});
+
+import { ref } from "vue";
+
+const isMenuOpen = ref(false);
+
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value;
+};
+</script>
+
+<template>
+  <div class="header">
+    <div class="hamburger" @click="toggleMenu">
+      <i class="fa fa-bars"></i>
+    </div>
+    <div class="image-holder">
+      <img class="image-holder__logo" src="../assets/logo.webp" alt="logo" />
+    </div>
+
+    <ul class="nav-links" :class="{ open: isMenuOpen }">
+      <li class="nav-links__home">Home</li>
+      <li class="nav-links__products">Products</li>
+      <li class="nav-links__contactUs">Contact Us</li>
+    </ul>
+    <div class="icons-holder">
+      <button class="icons-holder__signUp-btn">Sign Up</button>
+      <button class="icons-holder__cart-btn" style="font-size: 24px">
+        <i class="fa fa-shopping-cart"></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@media (max-width: 1023px) {
+  .header {
+    background-color: bisque;
+    display: flex;
+    justify-content: space-around;
+  }
+  .image-holder__logo {
+    max-width: 50px;
+    max-height: 50px;
+  }
+  .nav-links {
+    display: flex;
+    flex-direction: column;
+    list-style-type: none;
+  }
+  .hamburger {
+    display: flex;
+    cursor: pointer;
+    align-items: center;
+    font-size: 24px;
+    justify-content: center;
+  }
+  .nav-links {
+    display: none;
+    position: absolute;
+    left: 1rem;
+    top: 3rem;
+    background-color: white;
+  }
+  .nav-links.open {
+    display: flex;
+  }
+  .icons-holder {
+    display: flex;
+    gap: 20px;
+  }
+  .icons-holder__signUp-btn {
+    border: 2px solid black;
+    border-radius: 3px;
+    background: none;
+  }
+  .icons-holder__cart-btn {
+    background: none;
+    border: none;
+    size: 40px;
+  }
+}
+@media (min-width: 1024px) {
+  .hamburger {
+    display: none;
+  }
+  .header {
+    background-color: bisque;
+    line-height: 1.5;
+    display: flex;
+    justify-content: space-around;
+    gap: 5px;
+  }
+  .image-holder__logo {
+    padding-inline-start: 5px;
+    max-width: 50px;
+    max-height: 50px;
+  }
+  .icons-holder {
+    display: flex;
+    align-items: center;
+    gap: 10px;
+  }
+
+  .icons-holder__cart-btn {
+    background: none;
+    border: none;
+    size: 40px;
+  }
+
+  .icons-holder__cart-btn:hover {
+    cursor: pointer;
+    color: grey;
+  }
+  .icons-holder__signUp-btn {
+    border: 2px solid black;
+    border-radius: 3px;
+    background: none;
+  }
+  .icons-holder__signUp-btn:hover,
+  .nav-links__home:hover,
+  .nav-links__products:hover,
+  .nav-links__contactUs:hover {
+    cursor: pointer;
+    color: grey;
+  }
+  .nav-links {
+    list-style: none;
+    display: flex;
+    gap: 130px;
+  }
+}
+</style>
